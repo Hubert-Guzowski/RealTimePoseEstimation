5571d4c15f566a537c7e6c7227400cc99f94c395
Filip Zybała <filip.zybala@gmail.com>
2021-05-30 21:13:32 +0200

    Rewrite main

---
+++ src/main.py
@@ -0,0 +1,212 @@
+import cv2
+import numpy as np
+from mesh import Mesh
+from model import Model
+from robustmatcher import RobustMatcher
+import utils
+import main_utils
+
+# Settings
+
+video_read_path = "samples/cpp/tutorial_code/calib3d/real_time_pose_estimation/Data/box.mp4"
+yml_read_path = "samples/cpp/tutorial_code/calib3d/real_time_pose_estimation/Data/cookies_ORB.yml"
+ply_read_path = "samples/cpp/tutorial_code/calib3d/real_time_pose_estimation/Data/box.ply"
+
+f = 55                          # focal length in mm
+sx = 22.3                       # sensor size           
+sy = 14.9                       # sensor size
+width = 640                     # image size
+height = 480                    # image size
+
+cam_params = {
+    "fx": width * f / sx,
+    "fy": height * f / sy,
+    "cx": width / 2,
+    "cy": height / 2
+}
+
+red = (0, 0, 255, 0)
+green = (0, 255, 0, 0)
+blue = (255, 0, 0, 0)
+yellow = (0, 255, 255, 0)
+
+
+# Robust Matcher parameters
+numKeyPoints = 2000
+ratioTest = .70
+fast_match = True
+
+# RANSAC parameters
+iterationsCount = 500
+reprojectionError = 6.0
+confidence = 0.99
+
+# Kalman Filter parameters
+minInliersKalman = 30
+
+# PnP parameters
+pnpMethod = None # TODO: Rewrite PnP
+featureName = "ORB"
+useFLANN = False
+
+# Save results
+saveDirectory = ""
+frameSave = None # TODO
+frameCount = 0
+
+displayFilteredPose = False
+
+# Initialization
+
+pnp_detection = None # TODO
+pnp_detection_est = None # TODO
+
+model = Model()
+model.load(yml_read_path)
+
+mesh = Mesh()
+mesh.load(ply_read_path)
+
+detector, descriptor = utils.createFeatures(featureName, numKeyPoints)
+rmatcher = RobustMatcher(detector, descriptor, utils.createMatcher(featureName, useFLANN), ratioTest, cv2.imread(model.__training_img_path))
+
+nStates = 18
+nMeasurements = 6
+nInputs = 0
+dt = 0.125
+
+KF = main_utils.initKalmanFilter(nStates, nMeasurements, nInputs, dt)
+
+good_measurement = False
+
+list_points3d_model = model.__list_points3d_in
+descriptors_model = model.__descriptors
+keypoints_model = model.__keypoints
+
+cv2.namedWindow("REAL TIME DEMO", cv2.WINDOW_KEEPRATIO)
+
+cap = cv2.VideoCapture()
+cap.open(video_read_path)
+
+# TODO: 186 - 199 Saving video
+
+tm = cv2.TickMeter()
+
+while cv2.waitKey(30) != 27:
+    ret, frame = cap.read()
+    if ret == 0:
+        break
+    tm.reset()
+    tm.start()
+    frame_vis = np.copy(frame)
+
+    if fast_match is True:
+        good_matches, keypoints_scene = rmatcher.fastRobustMatch(frame, descriptors_model, keypoints_model)
+    else:
+        good_matches, keypoints_scene = rmatcher.robustMatch(frame, descriptors_model, keypoints_model)
+
+    frame_matching = rmatcher.getImageMatching()
+
+    if frame_matching:
+        cv2.imshow("Keypoints matching", frame_matching)
+
+    list_points3d_model_match = []
+    list_points2d_scene_match = []
+
+    for match in good_matches:
+        point3d_model = list_points3d_model[match.trainIdx]
+        point2d_scene = keypoints_scene[match.queryIdx].pt
+        list_points3d_model_match.append(point3d_model)
+        list_points2d_scene_match.append(point2d_scene)
+
+
+    utils.draw2DPoints(frame_vis, list_points2d_scene_match, red) # TODO
+
+    good_measurement = False
+    list_points2d_inliers = []
+
+    if good_matches.size() >= 4:
+
+        inliers_idx = pnp_detection.estimatePoseRANSAC( 
+            list_points3d_model_match, 
+            list_points2d_scene_match,
+            pnpMethod,
+            iterationsCount, 
+            reprojectionError, 
+            confidence)
+
+        for n in inliers_idx:
+            point2d = list_points2d_scene_match[n]
+            list_points2d_inliers.append(point2d)
+
+        utils.draw2DPoints(frame_vis, list_points2d_inliers, blue)
+
+        if inliers_idx >= minInliersKalman:
+            translation_measured = pnp_detection.get_t_matrix()
+            rotation_measured = pnp_detection.get_R_matrix()
+            measurements = utils.fillMeasurements(measurements, translation_measured, rotation_measured)
+            good_measurement = True
+
+        translation_estimated, rotation_estimated = main_utils.updateKalmanFilter(KF, measurements)
+        pnp_detection_est.set_P_matrix(rotation_estimated, translation_estimated)
+
+    l = 5
+    pose_points2d = []
+    if not good_measurement or displayFilteredPose:
+        utils.drawObjectMesh(frame_vis, mesh, pnp_detection_est, yellow)
+        pose_points2d.append(pnp_detection_est.backproject3DPoint((0, 0, 0)))
+        pose_points2d.append(pnp_detection_est.backproject3DPoint((l, 0, 0)))
+        pose_points2d.append(pnp_detection_est.backproject3DPoint((0, l, 0)))
+        pose_points2d.append(pnp_detection_est.backproject3DPoint((0, 0, l)))
+        utils.draw3DCoordinateAxes(frame_vis, pose_points2d)
+    else:
+        utils.drawObjectMesh(frame_vis, mesh, pnp_detection, green)
+        pose_points2d.append(pnp_detection.backproject3DPoint((0, 0, 0)))
+        pose_points2d.append(pnp_detection.backproject3DPoint((l, 0, 0)))
+        pose_points2d.append(pnp_detection.backproject3DPoint((0, l, 0)))
+        pose_points2d.append(pnp_detection.backproject3DPoint((0, 0, l)))
+        utils.draw3DCoordinateAxes(frame_vis, pose_points2d)
+
+    tm.stop()
+
+    fps = 1 / tm.getTimeSec()
+    utils.drawFPS(frame_vis, fps, yellow)
+
+    detection_ratio = (inliers_idx.size() / good_matches.size()) * 100
+    utils.drawConfidence(frame_vis, detection_ratio, yellow)
+
+    inliers_int = inliers_idx.size()
+    outliers_int = good_matches.size() - inliers_idx.size()
+
+    text = "Found {} of {} matches".format(inliers_int, good_matches.size())
+    text2 = "Inliers: {} - Outliers: {}".format(inliers_int, outliers_int)
+    utils.drawText(frame_vis, text, green)
+    utils.drawText2(frame_vis, text2, red)
+
+    cv2.imshow("REAL TIME DEMO", frame_vis)
+
+    # TODO - 345:367 Saving video
+
+cv2.destroyWindow("REAL TIME DEMO")
+print("GOODBYE ...")
+
+
+        
+
+
+
+
+        
+
+
+
+    
+
+
+
+
+
+
+
+
+
---
+++ src/main_utils.py
@@ -0,0 +1,80 @@
+import cv2
+import numpy as np
+import utils
+
+def initKalmanFilter(nStates, nMeasurements, nInputs, dt):
+    KF = cv2.KalmanFilter(nStates, nMeasurements, nInputs)
+    KF.processNoiseCov = cv2.setIdentity(KF.processNoiseCov, 1e-5)
+    KF.measurementNoiseCov = cv2.setIdentity(KF.measurementNoiseCov, 1e-2)
+    KF.errorCovPost = cv2.setIdentity(KF.errorCovPost, 1)
+    dt2 = 0.5*dt*dt
+    transitionMatrix = np.identity(nStates)
+    transitionMatrix[0, 3] = dt
+    transitionMatrix[1, 4] = dt
+    transitionMatrix[2, 5] = dt
+    transitionMatrix[3, 6] = dt
+    transitionMatrix[4, 7] = dt
+    transitionMatrix[5, 8] = dt
+    transitionMatrix[0, 6] = dt2
+    transitionMatrix[1, 7] = dt2
+    transitionMatrix[2, 8] = dt2
+
+    transitionMatrix[9, 12] = dt
+    transitionMatrix[10, 13] = dt
+    transitionMatrix[11, 14] = dt
+    transitionMatrix[12, 15] = dt
+    transitionMatrix[13, 16] = dt
+    transitionMatrix[14, 17] = dt
+    transitionMatrix[9, 15] = dt2
+    transitionMatrix[10, 16] = dt2
+    transitionMatrix[11, 17] = dt2
+
+    KF.transitionMatrix = transitionMatrix
+
+    measurementMatrix = np.zeros((nMeasurements, nStates))
+    measurementMatrix[0, 0] = 1
+    measurementMatrix[1, 1] = 1
+    measurementMatrix[2, 2] = 1
+    measurementMatrix[3, 9] = 1
+    measurementMatrix[4, 10] = 1
+    measurementMatrix[5, 11] = 1
+
+    KF.measurementMatrix = measurementMatrix
+
+    return KF
+
+def updateKalmanFilter(KF, measurement):
+    translation_estimated = np.zeros(3, dtype=np.float64)
+    rotation_estimated = np.zeros((3, 3), dtype=np.float64)
+
+    predicition = KF.predict()
+    estimated = KF.correct(measurement)
+
+    translation_estimated[0] = estimated[0]
+    translation_estimated[1] = estimated[1]
+    translation_estimated[2] = estimated[2]
+
+    eulers_estimated = np.zeros(3, dtype=np.float64)
+    eulers_estimated[0] = estimated[9]
+    eulers_estimated[1] = estimated[10]
+    eulers_estimated[2] = estimated[11]
+
+    rotation_estimated = utils.euler2rot(eulers_estimated)
+
+    return translation_estimated, rotation_estimated
+
+def fillMeasurements(translation_measured, rotation_measured):
+    measurements = np.zeros(6, dtype=np.float64)
+    measured_eulers = utils.rot2euler(rotation_measured)
+
+    measurements[0] = translation_measured[0]
+    measurements[1] = translation_measured[1]
+    measurements[2] = translation_measured[2]
+    measurements[3] = measured_eulers[0]
+    measurements[4] = measured_eulers[1]
+    measurements[5] = measured_eulers[2]
+
+    return measurements
+
+
+
